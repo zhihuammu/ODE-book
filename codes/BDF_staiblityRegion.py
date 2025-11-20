4512f429fd382290d620d6e660f0d0d60a9a270a
Zhihua Ma <z.ma@mmu.ac.uk>
2025-11-20 10:10:14 +0000

    update the code

--- codes/BDF_staiblityRegion.py
+++ codes/BDF_staiblityRegion.py
@@ -46,3 +46,11 @@ ax.set_aspect('equal', adjustable='box')
 
 plt.show()
 
+# enter the coefficients of poly 
+# in the array
+p = np.poly1d([1, 2, 1])
+
+# multiplying by r(or roots) to 
+# get the roots
+root_of_poly = p.r
+print(root_of_poly)
