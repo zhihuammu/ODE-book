7ce16ca2c495fe80cfc41705ee450b2d78cc01c4
Zhihua Ma <z.ma@mmu.ac.uk>
2026-01-18 11:27:03 +0000

    update matlab codes for chapter 8 to Matlab 2025

--- codes/ch7_stiff_1.m
+++ codes/ch7_stiff_1.m
@@ -1,18 +1,23 @@
-%6G6Z3002 Computational Methods in ODEs - Chapter 7 (Matlab program 4)
-%Scriptfile to solve a system of three 1st order ODEs, using a stiff
-%solver, ode15s, from the MATLAB ODE solver routines.
+%Solve a system of three 1st order ODEs, using a stiff solver, ode15s, 
+% from the MATLAB ODE solver routines.
 
-function ch6_stiff_1
+%%1-> Define the IVP
+% declare an ode function (definition given at the end of the code)
+f=@odeFunc;
 
-y0= [1.0 0.0 0.0];
-tspan=[0,1.0e-5,1.0e-3,0.1,0.5,1,2,4,6,8,10,20,30,40,50,60]; 
+%set the initial values 
+y0= [1.0 0.0 0.0];  
 
-% Note: For a smooth curve use tspan=[0 60] 
-%options = odeset('RelTol',1e-6,'AbsTol',[1e-10,1e-18,1e-12]);
+%set specific output points
+tspan=[0,1.0e-5,1.0e-3,0.1,0.5,1,2,4,6,8,10,20,30,40,50,60];  
 
-options = odeset('RelTol',1e-6,'AbsTol',1e-6);
-[t,y]=ode15s(@ch6f3,tspan,y0,options);
+%%2-> Set options
+options = odeset('RelTol',1e-6,'AbsTol',1e-6); %set tolerances
 
+%%3-> Call the ODE solver routine ode15s
+[t,y]=ode15s(@odeFunc,tspan,y0,options);
+
+%%4-> Output the result
 disp('     i      t         y1         y2         y3 ');
 fprintf(' -----------------------------------------\n');
 
@@ -31,10 +36,11 @@ semilogy(t,y(:,1),t,y(:,2),t,y(:,3),'LineWidth',2);
 title(['Solution of the Robertson Problem for t=0:0.01s']);
 xlabel('time (Sec)'); ylabel('solution y');
 axis([tspan(1) 0.01 1.0e-12 10]);  
-
-% ---------------------------------------------------------------
-function f = ch6f3(t,y)
+% --------------------------------------------------------------------------
+% Defining the ODE problem (or function) to be solved
+function f = odeFunc(t,y)
 	f=zeros(3,1);
 	f(1)= -0.04*y(1)+1.0e4*y(2)*y(3);
 	f(2)= 0.04*y(1)-1.0e4*y(2)*y(3)-3.0e7*y(2)*y(2);
 	f(3)= 3.0e7*y(2)*y(2);
+end
--- codes/test.m
+++ codes/test.m
@@ -1,32 +1,46 @@
+%Solve a system of three 1st order ODEs, using a stiff solver, ode15s, 
+% from the MATLAB ODE solver routines.
+
 %%1-> Define the IVP
 % declare an ode function (definition given at the end of the code)
 f=@odeFunc;
 
-y0=1.0;  %set the initial value of the dependent variable y
-tspan=[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]; %set specific output point
+%set the initial values 
+y0= [1.0 0.0 0.0];  
+
+%set specific output points
+tspan=[0,1.0e-5,1.0e-3,0.1,0.5,1,2,4,6,8,10,20,30,40,50,60];  
 
 %%2-> Set options
 options = odeset('RelTol',1e-6,'AbsTol',1e-6); %set tolerances
 
-%%3-> Call the ODE solver routine ode113
-[t,y]=ode113(@odeFunc,tspan,y0,options); 
-
+%%3-> Call the ODE solver routine ode15s
+[t,y]=ode15s(@odeFunc,tspan,y0,options);
 
 %%4-> Output the result
-disp('  t        y          f(t)      y_ex     Abs_error ');
+disp('     i      t         y1         y2         y3 ');
+fprintf(' -----------------------------------------\n');
 
-for i=1:length(t)  %in this case length(t)=11, from tspan
-    yex(i)=exp(-t(i))+t(i);  %calculating the exact solutions at each step
-    abserror(i)=abs(yex(i)-y(i));  %calculating the absolute error
-    fprintf('%5.2f %10.7f %10.7f %10.7f %11.8f\n', t(i),y(i),f(t(i),y(i)),yex(i),abserror(i));
+for i=1:length(t)
+	fprintf('%5i %10.2e %10.2e %10.2e %10.2e\n',i,t(i),y(i,1),y(i,2),y(i,3));
 end
 
-plot(t,y(:,1),'LineWidth',2);
-title(['Solution of dy/dt = -y+t+1, y0 = ' num2str(y0),', using ode113']);
-xlabel('t');
-ylabel('solution y');
+figure(1)
+semilogy(t,y(:,1),t,y(:,2),t,y(:,3),'LineWidth',2);
+title(['Solution of the Robertson Problem for t=0:0.60s']);
+xlabel('time (Sec)'); ylabel('solution y');
+axis([tspan(1) tspan(end) 1.0e-12 10]);  
+
+figure(2)
+semilogy(t,y(:,1),t,y(:,2),t,y(:,3),'LineWidth',2);
+title(['Solution of the Robertson Problem for t=0:0.01s']);
+xlabel('time (Sec)'); ylabel('solution y');
+axis([tspan(1) 0.01 1.0e-12 10]);  
 % --------------------------------------------------------------------------
 % Defining the ODE problem (or function) to be solved
-function f=odeFunc(t,y)
-    f=-y+t+1;
+function f = odeFunc(t,y)
+	f=zeros(3,1);
+	f(1)= -0.04*y(1)+1.0e4*y(2)*y(3);
+	f(2)= 0.04*y(1)-1.0e4*y(2)*y(3)-3.0e7*y(2)*y(2);
+	f(3)= 3.0e7*y(2)*y(2);
 end
